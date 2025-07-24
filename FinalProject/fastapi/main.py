f4e5f439daa541638df5021b28deb0a262a5b396
Michael Sweeney <sweeneym11@wit.edu>
2025-07-24 14:13:24 -0400

    updated fastapi

--- FinalProject/fastapi/main.py
+++ FinalProject/fastapi/main.py
@@ -1,7 +1,9 @@
 # app/main.py
 from fastapi import FastAPI
+from routers import store
 
 app = FastAPI()
+#app.include_router(store.router)
 
 @app.get("/")
 def read_root():
---
+++ FinalProject/fastapi/models/database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "mysql+pymysql://root:@localhost:3306/accessory_shop"
+
+#If FastAPI runs in Docker and MySQL is outside Docker, use "host.docker.internal" instead of "localhost".
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+Base = declarative_base()
