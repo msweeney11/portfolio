0559a7613ae93db1d3cf09970d81977be7855098
Michael Sweeney <sweeneym11@wit.edu>
2025-08-03 18:12:29 -0400

    created log in page

--- FinalProject/fastapi/main.py
+++ FinalProject/fastapi/main.py
@@ -1,9 +1,19 @@
 # app/main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-#app.include_router(store.router)
+
+# Add CORS middleware
+app.add_middleware(  # type: ignore
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
     return {"message": "FastAPI service is working!"}
+
